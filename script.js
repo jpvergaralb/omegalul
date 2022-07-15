85c7a3c651b245ffb9d4c32506403a40591593f3
jpvergaralb <jpvergaralb@uc.cl>
2022-07-15 17:20:45 -0400

    add buttons

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+
+// Buttons
 const closeButton = document.querySelector(".close-button")
+const homeButton = document.querySelector(".home-button")
+const githubButton = document.querySelector(".github-button")
+const spotifyButton = document.querySelector(".spotify-button")
+const catsButton = document.querySelector(".cats-button")
+const snakeButton = document.querySelector(".snake-button")
+const aboutButton = document.querySelector(".about-button")
+
+githubButton.addEventListener("click", () => {
+  const link = "http://localhost:8000/"
+  window.location = link
+})
+
+spotifyButton.addEventListener("click", () => {
+  const link = "http://localhost:8000/"
+  window.location = link
+})
+
+catsButton.addEventListener("click", () => {
+  const link = "http://localhost:8000/"
+  window.location = link
+})
+
+snakeButton.addEventListener("click", () => {
+  const link = "http://localhost:8000/views/snake.html"
+  window.location = link
+})
+
+aboutButton.addEventListener("click", () => {
+  const link = "http://localhost:8000/views/about.html"
+  window.location = link
+})
 
 closeButton.addEventListener("click", () => {
   const link = "https://www.youtube.com/watch?v=ncWgoD2MDnQ"
   window.location = link
 })
+homeButton.addEventListener("click", () => {
+  const link = "http://localhost:8000/"
+  window.location = link
+})
+
+// Buttons

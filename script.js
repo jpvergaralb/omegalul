917fff2122b549ae91ed56c48bfb6ba37ff76d27
jpvergaralb <jpvergaralb@uc.cl>
2022-07-15 14:44:40 -0400

    trying new things

---
+++ script.js
@@ -0,0 +1,6 @@
+const closeButton = document.querySelector(".close-button")
+
+closeButton.addEventListener("click", () => {
+  const link = "https://www.youtube.com/watch?v=ncWgoD2MDnQ"
+  window.location = link
+})
